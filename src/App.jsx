ddddef2ed7161efd7a0faed4aa514136186b0cf4
Buianhvu226 <buianhvu226@gmail.com>
2024-09-30 23:41:26 +0700

    Login_Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Footer from "./layouts/footer/Footer"
 import Header from "./layouts/header/Header"
 // import Page from "./layouts/login_signup/Page"
 // import Login from "./pages/login/Login"
-import Start from "./pages/start/start"
+// import Start from "./pages/start/start"
+import Login_Page from "./pages/login/Login_Page"
 
 function App() {
 
   return (
     <div className="overflow-hidden">
       <Header />
-      <Start />
+      {/* <Start /> */}
+      <Login_Page />
       <Footer />
     </div>
   )
