a28b4c444fbc26bdc857ebb6d6732d2537ba65f4
Hoang Nguyen <freddyfrank69@gmail.com>
2024-10-03 11:32:44 +0700

    Adding Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Register from "./pages/register/register";
 import ForgotPasswordPage from "./pages/login/ForgotPasswordPage";
 import CreateNewPassword from "./pages/login/CreateNewPassword";
 import Login from "./pages/login/Login";
-import Start from "./pages/start/Start"
+import Start from "./pages/start/Start";
+import Panel from "./pages/panel/Panel";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/verifyEmail' element={<VerifyEmail />} />
         <Route path='/forgotPassword' element={<ForgotPasswordPage />} />
         <Route path='/createNewPassword' element={<CreateNewPassword />} />
+        <Route path='/panel' element={<Panel />} />
       </Routes>
       <Footer />
     </div>
