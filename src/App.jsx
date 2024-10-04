6186c44a7deda58aa0014cf3c2fe6384858f6234
Buianhvu226 <buianhvu226@gmail.com>
2024-10-04 20:41:57 +0700

    Add SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 import AppProvider from "./AppProvider";
-import Footer from "./layouts/footer/Footer"
-import Header from "./layouts/header/Header"
+import Footer from "./layouts/footer/Footer";
+import Header from "./layouts/header/Header";
 import VerifyEmail from "./pages/login/VerifyEmail";
 import Register from "./pages/register/register";
 import ForgotPasswordPage from "./pages/login/ForgotPasswordPage";
 import CreateNewPassword from "./pages/login/CreateNewPassword";
 import Login from "./pages/login/Login";
-import Start from "./pages/start/Start"
+import Start from "./pages/start/Start";
+import SideBar from "./layouts/side_bar/SideBar";
 
 function App() {
   return (
@@ -19,19 +19,19 @@ function App() {
       <ToastContainer />
       <div className="overflow-hidden">
         <Header />
-        <Routes>
+        {/* <Routes>
           <Route path='/' element={<Start />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/verifyEmail' element={<VerifyEmail />} />
           <Route path='/forgotPassword' element={<ForgotPasswordPage />} />
           <Route path='/createNewPassword' element={<CreateNewPassword />} />
-        </Routes>
+        </Routes> */}
+        <SideBar />
         <Footer />
       </div>
     </AppProvider>
-
-  )
+  );
 }
 
 export default App;
