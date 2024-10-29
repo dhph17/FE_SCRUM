125e5e07a5d178ba108b9f58d8463f868ef85311
dhph17 <156217401+dhph17@users.noreply.github.com>
2024-10-29 23:22:16 +0700

    Done report post

--- src/layouts/itemPost/ItemPost.jsx
+++ src/layouts/itemPost/ItemPost.jsx
@@ -7,12 +7,15 @@ import Img3 from "../../assets/image/medal.png";
 import User from '../../assets/image/User.png'
 import ClassTimeDetail from "../../layouts/popup/classTime_Popup";
 import { useAppContext } from "../../AppProvider";
+import ReportContent from "../popup/reportContent";
 
 const ItemPostVu = ({ user, children, tag }) => {
     const [tagPost, setTagPost] = useState()
     const [selectedTime, setSelectedTime] = useState(null);
+    const [showReport, setShowReport] = useState(false)
+    const [showReportContent, setShowReportContent] = useState(false)
 
-    const { role } = useAppContext();
+    const { role, id } = useAppContext();
 
     const handleBuoiHocClick = (post) => {
         setSelectedTime(post);
@@ -20,6 +23,11 @@ const ItemPostVu = ({ user, children, tag }) => {
 
     const handleClosePopup = () => {
         setSelectedTime(null);
+    };
+
+    const handleBackgroundClick = () => {
+        setShowReportContent(false);
+        setShowReport(false)
     };
 
     useEffect(() => {
@@ -46,27 +54,70 @@ const ItemPostVu = ({ user, children, tag }) => {
 
     return (
         <div>
-            <div className="flex justify-between p-4">
-                <div className="flex gap-5 ">
-                    <img
-                        className="w-[50px] h-[50px] rounded-full"
-                        src={user.avatar ? `${import.meta.env.VITE_API_ENDPOINT}/${user.avatar}` : User}
-                        alt="avatar"
-                    />
-                    <div>
-                        <strong>{user.parent_name || user.username}</strong>
-                        {
-                            role === 'admin' && tag === "Đã phê duyệt" ?
-                                (
-                                    <p className="opacity-60">{formatDate(user.created_at)}. Được duyệt lúc {formatDate(user.last_updated)}</p>
-                                )
-                                :
-                                (
-                                    <p className="opacity-60">{formatDate(user.created_at)}</p>
-                                )
-                        }
+            <div
+                className="flex justify-between p-4 group"
+            >
+                <div className="flex items-center gap-7">
+                    <div className="flex gap-5 ">
+                        <img
+                            className="w-[50px] h-[50px] rounded-full"
+                            src={user.avatar ? `${import.meta.env.VITE_API_ENDPOINT}/${user.avatar}` : User}
+                            alt="avatar"
+                        />
+                        <div>
+                            <strong>{user.parent_name || user.username}</strong>
+                            {
+                                role === 'admin' && tag === "Đã phê duyệt" ?
+                                    (
+                                        <p className="opacity-60">{formatDate(user.created_at)}. Được duyệt lúc {formatDate(user.last_updated)}</p>
+                                    )
+                                    :
+                                    (
+                                        <p className="opacity-60">{formatDate(user.created_at)}</p>
+                                    )
+                            }
+                        </div>
                     </div>
+                    {
+                        id !== user.parent_id && (
+                            <div className="">
+                                <i
+                                    className={`fa-solid fa-ellipsis text-2xl cursor-pointer hover:text-black transition-all du ${!showReport && !showReportContent ? 'text-slate-100' : 'text-black'}`}
+                                    onClick={() => { setShowReport(!showReport); setShowReportContent(false) }}
+                                ></i>
+
+                                {
+                                    showReport && (
+                                        <div className="bg-slate-100 absolute  p-1 rounded-md cursor-pointer shadow-md">
+                                            <div
+                                                className="flex gap-1 items-center p-1 hover:bg-slate-200 rounded-md"
+                                                onClick={() => { setShowReport(false); setShowReportContent(true) }}
+                                            >
+                                                <i className="fa-solid fa-flag"></i>
+                                                <span>Report</span>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                {showReportContent && (
+                                    <div className="fixed inset-0 bg-black bg-opacity-30 z-40" onClick={handleBackgroundClick}></div>
+                                )}
+                                {
+                                    showReportContent && (
+                                        <div className="fixed inset-0 flex items-center justify-center z-40">
+                                            <ReportContent
+                                                onClose={() => setShowReportContent(false)}
+                                                reportedPartyId={user.parent_id}
+                                                postId={user.post_id}
+                                            />
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </div>
+
                 <div className="flex">
                     <img className="w-[22px] h-[22px] mr-1" src={tagPost} alt="" />
                     <p className="font-semibold">{tag}</p>
